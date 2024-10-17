ec1f396c8d5943307cb171691e92c52ea03218d7
Andresyt12 <andresfelipeosorioperez894@gmail.comm>
2024-10-17 13:37:54 -0500

    Construyendo de nuevo el proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,39 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import './pages/Egresos.css'
 
-render(<App />, document.getElementById('app'))
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
+import './pages/Home.css';
+import Egresos from './pages/Egresos'
+
+let router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/Egresos',
+    element: <Egresos />
+  }
+])
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
